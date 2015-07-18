00277c16c06acb7a86ee909bd0bb102c7c346163
cct3000 <meganet001@gmail.com>
2015-07-18 17:39:01 +0800

    Read File from internal memory

--- src/com/example/try3000/MainActivity.java
+++ src/com/example/try3000/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.try3000;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
@@ -26,6 +30,36 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         addSaveButtonListener();
+        
+        loadSaveFile();
+    }
+    
+    private void loadSaveFile(){
+    	
+    	try {
+			FileInputStream fis = openFileInput(TEXTFILE);
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					new DataInputStream(fis)));
+			
+			EditText editText = (EditText) findViewById(R.id.text);
+			
+			String line;
+			while((line = reader.readLine()) != null){
+				editText.append(line);
+				editText.append("\n");
+			}
+			
+			fis.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			Log.d(DEBUGTAG, "Unable to read file");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
     private void addSaveButtonListener(){
     	Button saveBtn = (Button) findViewById(R.id.save);
